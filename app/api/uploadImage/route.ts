406b33ad875c42bbf7a3f867bc4bb4cb107af493
Andrei Cosmin Radu <andreicos1@yahoo.com>
2023-10-07 23:00:20 +0300

    add decoding for key

--- app/api/uploadImage/route.ts
+++ app/api/uploadImage/route.ts
@@ -23,7 +23,7 @@ export const POST = async (request: NextRequest, response: NextResponse) => {
         type: "service_account",
         project_id: "ai-memegen",
         private_key_id: process.env.GCS_PRIVATE_KEY_ID,
-        private_key: process.env.GCS_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+        private_key: Buffer.from(process.env.GCS_PRIVATE_KEY!, "base64").toString().replace(/\\n/g, "\n"),
         client_email: process.env.GCS_CLIENT_EMAIL,
         client_id: process.env.GCS_CLIENT_ID,
         auth_uri: "https://accounts.google.com/o/oauth2/auth",
